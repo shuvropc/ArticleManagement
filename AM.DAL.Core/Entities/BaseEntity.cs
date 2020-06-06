3753f15476c697a42f606fd2d62588c3b9d76484
shuvropc <shuvro.pc.1994@gmail.com>
2020-06-06 12:06:14 +0600

    Entity Added

---
+++ AM.DAL.Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AM.DAL.Core.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DateCreated { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime? DateModified { get; set; }
+        public long? ModifiedBy { get; set; }
+    }
+}
